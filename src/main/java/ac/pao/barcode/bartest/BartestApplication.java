7fcea6130f923c82239f94e4aea796823075c674
kitou88C39 <kanfutrooper@icloud.com>
2023-05-24 16:11:26 +0900

    first commit

---
+++ src/main/java/ac/pao/barcode/bartest/BartestApplication.java
@@ -0,0 +1,13 @@
+package ac.pao.barcode.bartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/pao/barcode/bartest/BartestApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.pao.barcode.bartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
